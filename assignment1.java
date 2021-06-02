494e18bc824ab57e9203d22ff27df2d474c1857a
BrendonGsmoothstack <85197250+BrendonGsmoothstack@users.noreply.github.com>
2021-06-01 20:53:03 -0400

    Add files via upload

--- assignment1.java
+++ assignment1.java
@@ -2,29 +2,29 @@ package Smoothstack;
 
 public class assignment1 {
     public static void main(String[] args) {
-        // PROBLEM #1
-        // Initialize string to be output with one "*"
-        String stars = "*";
 
-        // Run a for-loop 4 times that prints out the string and 
-        // then adds another "*" onto it.
+        //---PROBLEM #1---//
+
+        /* Initialize string to be output with one "*" then run a for-loop 4 times that 
+        prints out the string and continually adds an additional "*" onto it. Print out 
+        a line of 9 dots at the end. */
+        String starsP1 = "*";
+
         for (int i = 0; i < 4; i++) {
-            System.out.println(stars);
-            stars += "*";
+            System.out.println(starsP1);
+            starsP1 += "*";
         }
 
-        // Print out one line of dots after the loop is done. 
         System.out.println(".........");
 
+        //---PROBLEM #2---//
 
-        //PROBLEM #2
-
-        // Print out one line of dots at the start.
+        /* Print out a line of 10 dots at the start and run a double for-loop to print out "*". 
+        The number of stars decreases with each iteration. The value of the inner loop counter 'j' 
+        is dependent on the value of the outer loop 'i'. The function starts by printing 4 stars 
+        and then decreases to 3, 2, and 1 stars. */
         System.out.println("..........");
 
-        // Double for-loop to print out "*". Strings are immutable and cannot be shortened so this
-        // function prints out 4 stars with the inner loop, then increments the outer loop by 1, starts
-        // the inner loop at a higher value, and prints out 3, 2, and 1 stars. 
         for (int i = 0; i < 4; i++) {
             for (int j = i; j < 4; j++) {
                 System.out.print("*");
@@ -32,16 +32,15 @@ public class assignment1 {
             System.out.println();
         }
 
+        //---PROBLEM #3---//
 
-        // PROBLEM #3
-        
-        // Initialize string variable with one star. 
+        /* Initialize string variable with one star. Run a double for-loop that runs 4 times. 
+        Inner loop counter 'j' depends on the outer loop counter 'i'. The inner loop prints out 
+        spaces and the number of spaces printed decreases with each iteration. Inner loop prints 
+        out 5, 4, 3, and then 2 spaces. Outer for-loop prints the string and then concatenates it
+        with 2 more stars before running again. At the end, print out a line of 11 dots. */
         String starsP3 = "*";
 
-        // Run a double for-loop that runs 4 times. Inner for-loop prints spaces
-        // based on the counter of the outer loop. Inner for-loop prints 5, 4, 3, 
-        // then 2 spaces. Outer for-loop prints the starsP3 string and then concatenates
-        // it with 2 more stars before running again. 
         for (int i = 0; i < 4; i++) {
 
             for (int j = i; j < 5; j++) {
@@ -55,8 +54,12 @@ public class assignment1 {
 
         System.out.println("...........");
 
+        //---PROBLEM #4---//
 
-        // PROBLEM #4
+        /* Print out a line of 12 dots. Run a for-loop with 2 nested for-loops inside of it.
+        The first nested loop prints out spaces, the second one prints out "*". The counters
+        'j' and 'k' for both loops depend on the counter of the outer loop 'i'. The first loop
+        prints out 2, 3, 4, then 5 spaces. The second loop prints out 7, 5, 3, then 1 stars. */
 
         System.out.println("............");
         
--- assignment2.java
+++ assignment2.java
@@ -1,58 +1,65 @@
 package Smoothstack;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.InputMismatchException;
 
 public class assignment2 {
     public static void main(String[] args) {
 
         /* Create a scanner to receive user input, a random number generator, and
-        an int to store the random number between 1-100. */
+        an int to store the random number generated between 1-100. */
         Scanner input = new Scanner(System.in);
         Random ran = new Random();
         int randomInt = ran.nextInt(101);
 
-        /* Receive the initial guess from the user and check if the number is within
-        the bounds of 1-100. If it is not, continually print an error statement and 
-        request a new valid integer between 1-100. While loop only runs if the initial
-        answer is out of bounds. */
-        System.out.print("Guess a number between 1-100: ");
-        int userGuess = input.nextInt();
-        while (!(userGuess >= 1 && userGuess <= 100)) {
-            System.out.print("INVALID NUMBER. Choose a number between 1-100: ");
-            userGuess = input.nextInt();
-        }
-
-        /* Run a while-loop that runs a maximum of 5 times. This while loop always runs
-        at least once and first checks whether the initial guess is within +/- 10 of the
-        correct answer. If it is, it prints out a statement and breaks the while loop. If 
-        it is not, and the user has guess 5 times, then it prints a statement and breaks
-        the loop. 
-        
-        Otherwise, the loop prints a prints a statement and requests a new guess from the user.
-        If the user enters an invalid number then an error message continually prints until a 
-        valid number is entered. The counter (attempt) is then incremented by 1.
-        */
+        /* Give the input variable 'userGuess' an initial value of 0, set the do-while condition
+        validInt to false, and set the attempts to '1'. Print the first statement asking for the
+        first guess from the user. */
+        int userGuess = 0;
+        boolean validInt = false;
         int attempt = 1;
+        System.out.print("Guess a number between 1-100: ");
+
+        /* Run a while loop a maximum of 5 times for the 5 guesses by the user. A do-while with a 
+        try-catch is nested inside at the start to make sure the user enters a valid integer. If
+        the user enters a value that is not of int-type then a mismatch exception catches the 
+        error and runs the do-while again. If the user enters an integer between 1-100 then the 
+        while condition becomes true which ends the loop. If the integer is invalid, the loop continues. */
         while (attempt <= 5) {
+            do {
+                try {
+                    userGuess = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.print("INVALID TYPE. Choose an integer between 1-100: ");
+                    input.next();
+                    continue;
+                }
+                if (userGuess >= 1 && userGuess <= 100) {
+                    validInt = true;
+                }
+                else {
+                    System.out.print("INVALID NUMBER. Choose an integer between 1-100: ");
+                }
+            } while (validInt == false);
+
+            /* If the user's guess is within 10 of the correct answer, print a statement and break 
+            the while loop. If it is not, print a request for a new value and increment 'attempt' by 1. */
             if (Math.abs(userGuess - randomInt) <= 10) {
                 System.out.println("Correct!");
                 break;
             }
-            else if (attempt == 5) {
-                System.out.println("Sorry! The answer was " + randomInt + ".");
-                break;
-            }
 
             System.out.print(userGuess + " is not within +/- 10 of the answer. Try again: ");
-            userGuess = input.nextInt();
-            while (!(userGuess >= 1 && userGuess <= 100)) {
-                System.out.print("INVALID NUMBER. Choose a number between 1-100: ");
-                userGuess = input.nextInt();
-            }
             attempt++;
         }
 
-        /* Scanner 'input' is finally closed. */
+        // If the correct answer was not guessed in 5 attempts, print the following statement. 
+        if (attempt == 6) {
+            System.out.println("Sorry! The answer was " + randomInt + ".");
+        }
+
+        // Scanner 'input' is closed at the end. 
         input.close();
     }
 }
