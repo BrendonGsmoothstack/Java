c4a8d8a4d1182ee983f837eb3a511c24132c7136
BrendonGsmoothstack <85197250+BrendonGsmoothstack@users.noreply.github.com>
2021-06-07 14:20:11 -0400

    Add files via upload

--- JBWK1_A6.java
+++ JBWK1_A6.java
@@ -10,13 +10,44 @@ public class JBWK1_A6 {
     public static class SampleSingleton {
 
         private static Connection conn = null;
-        private static SampleSingleton instance = null;
+
+        private static volatile SampleSingleton instance = null;
         
+        private SampleSingleton() {}
+
         public static SampleSingleton getInstance() {
             if (instance == null) 
-                instance = new SampleSingleton();
+                synchronized(SampleSingleton.class) {
+                    if (instance == null) {
+                        instance = new SampleSingleton();
+                    }
+                }
 
             return instance;
         }
     }
+
+    // public class DatabaseConnection {
+        
+    //     private static Connection con = null;
+
+    //     static {
+    //         String url = "jdbc:mysql:// localhost:3306/org";
+    //         String user = "root";
+    //         String pass = "root";
+
+    //         try {
+    //             Class.forName("com.mysql.jdbc.Driver");
+    //             con = DriverManager.getConnection(url, user, pass);
+    //         }
+    //         catch (ClassNotFoundException | SQLException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+    //     public static Connection getConnection() {
+    //         return con;
+    //     }
+    // }
 }
+
+
